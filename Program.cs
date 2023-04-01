5b72b10d8311e44c21bcb160bfed6ea15245f9eb
wjdals819 <wjdals819@github.com>
2023-04-01 15:47:13 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_ConstEnumNullableVar
+{
+    class Program
+    {
+        enum Weapon { SWORD, STAFF, GUN, AXE, BOW };
+
+        static void Main(string[] args)
+        {
+            int? inv = null;
+            Console.WriteLine(inv.HasValue);
+
+            Weapon select = Weapon.SWORD;
+
+            inv = (int)select;
+            Console.WriteLine(inv.HasValue);
+            Console.WriteLine(inv.Value);
+
+        }
+    }
+}
